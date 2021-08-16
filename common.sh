1646198210052c64a29736333bb3e60cb6564cfa
Vít Novotný <witiko@mail.muni.cz>
2021-08-16 12:01:49 +0200

    Remove dead code

--- common.sh
+++ common.sh
@@ -9,19 +9,6 @@ then
     exit 1
 fi
 
-download_wmt13() {
-    (
-        set -e
-        trap 'rm -rf data/wmt13/' EXIT
-        echo Downloading wmt13 dataset to data/wmt13
-        mkdir -p data/wmt13
-        wget -q --show-progress 'http://www.statmt.org/wmt13/training-monolingual-news-2011.tgz' -O - | tar xzC data/wmt13/
-        wget -q --show-progress 'http://www.statmt.org/wmt13/training-monolingual-nc-v8.tgz'     -O - | tar xzC data/wmt13/
-        wget -q --show-progress 'http://www.statmt.org/wmt13/training-monolingual-news-2012.tgz' -O - | tar xzC data/wmt13/
-        trap '' EXIT
-    )
-}
-
 download_wikipedia_dump() {
     (
         set -e
